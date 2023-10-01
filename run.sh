facb9932102b072a67e61222462fdaa41d6f3f85
Maik Lorenz <lorenz.maik@web.de>
2023-10-01 12:41:44 +0200

    Modify run.sh: Open HomeAssistant in Chromium Browser

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 #-----------------------------------------------------
 # Run containers
-docker-compose up deconz homeassistant portainer
+docker-compose up deconz homeassistant portainer -d
+DISPLAY=:0.0 chromium-browser --kiosk http://127.0.0.1:8123/
